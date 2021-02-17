8bffa0a574eca0709496c5333d8cb4b3cdf01a56
Orlan Quijada <orlantquijada@gmail.com>
2021-02-17 18:22:55 +0800

    hax to read multiple lines

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ from lib.lexer import Lexer
 
 
 def main():
-    text = input("cfpl>")
-    lexer = Lexer(text)
-    while lexer.current_char is not None:
-        print(lexer.get_next_token())
+    # hax for presentation
+    with open("./sample-source-codes/1.cfpl", "r") as file:
+        for line in file.readlines():
+            lexer = Lexer(line)
+            while lexer.current_char is not None:
+                print(lexer.get_next_token())
 
 
 if __name__ == "__main__":
